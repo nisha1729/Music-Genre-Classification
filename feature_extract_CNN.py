19c43567473f00de0c41acc14d50574a98c0aa6b
Nisha George <nishageorge1729@gmail.com>
2020-10-15 20:05:18 +0200

    CNN for classification from audio files, features can be extracted from one of the conv layers

--- feature_extract_CNN.py
+++ feature_extract_CNN.py
@@ -7,20 +7,18 @@ import sys
 import torch
 import numpy as np
 import torch.nn as nn
-from pydub import AudioSegment
 from torch.utils.data import Dataset
 from torchsummary import summary
-from sklearn.preprocessing import LabelEncoder
 from scipy.io.wavfile import read
 
-
 folderNam = 'dataset_clips'
-batch_size = 5
+batch_size = 100
 learning_rate = 2e-4
 reg = 0.01
 num_epochs = 10
 
 
+# Data loader class
 class AudioLoader(Dataset):
     def __init__(self, dataset_path='dataset_clips'):
         self.audio_list = []
@@ -40,39 +38,64 @@ class AudioLoader(Dataset):
     def __getitem__(self, idx):
         # print(self.audio_list[idx])
         audio = read((self.audio_list[idx]))
-        print('audio', audio.shape)
+        # print(audio[1])
+        # print(np.size(audio[1]))
         label = self.label_dict[os.path.dirname(self.audio_list[idx]).split('\\')[1][:-4]]
-        return audio, label
+        return audio[1], label
 
 
+# Model definition
 class CNN_features(nn.Module):
     def __init__(self):
         super(CNN_features, self).__init__()
 
         self.cnn_layers = nn.Sequential(
             # Defining a 2D convolution layer
-            nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(4),
+            nn.Conv1d(2, 4, kernel_size=3, stride=1, dilation=1),
+            # nn.BatchNorm2d(4),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            # Defining another 2D convolution layer
-            nn.Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(4),
+            nn.MaxPool1d(kernel_size=5, stride=5),
+            # # Defining another 2D convolution layer
+            nn.Conv1d(4, 10, kernel_size=3, stride=1, dilation=3),
+            # nn.BatchNorm2d(4),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool1d(kernel_size=5, stride=5),
+            # # Defining another 2D convolution layer
+            nn.Conv1d(10, 15, kernel_size=3, stride=1, dilation=9),
+            # nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=5, stride=5),
+            nn.Conv1d(15, 20, kernel_size=3, stride=1, dilation=27),
+            # nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=5, stride=5),
+            nn.Conv1d(20, 25, kernel_size=3, stride=1, dilation=81),
+            # nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(25, 30, kernel_size=3, stride=1, dilation=243),
+            # nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=5, stride=5),
         )
 
         self.linear_layers = nn.Sequential(
-            nn.Linear(4 * 7 * 7, 10)
+            nn.Linear(2910, 200),
+            nn.ReLU(inplace=True),
+            nn.Linear(200, 4),
+            nn.Softmax(dim=1)
         )
 
     # Defining the forward pass
     def forward(self, x):
-        x = self.cnn_layers(x)
+
+        x = self.cnn_layers(x.permute(0,2,1))
         x = x.view(x.size(0), -1)
         x = self.linear_layers(x)
         return x
 
+
+# Load the data
 audio_loader = AudioLoader()
 
 train_loader = torch.utils.data.DataLoader(audio_loader,
@@ -83,66 +106,29 @@ print(summary(model))
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=reg)
 
-
 # Train the model
 lr = learning_rate
 total_step = len(train_loader)
 for epoch in range(num_epochs):
+    correct = 0
+    total = 0
     for i, data in enumerate(train_loader):
         (audio, labels) = data
-#         # print(labels)
-        sys.exit(0)
-#
-#         # Forward pass
-#         outputs = model(audio)
-#
-#         loss = criterion(outputs, labels)
-#
-#         # Backward and optimize
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#         if (i+1) % 100 == 0:
-#             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-#                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-#
-#     # Code to update the lr
-#     lr *= learning_rate_decay
-#     update_lr(optimizer, lr)
-#     model.eval()
-#     with torch.no_grad():
-#         correct = 0
-#         total = 0
-#         for images, labels in val_loader:
-#             images = images.to(device)
-#             labels = labels.to(device)
-#             outputs = model(images)
-#             _, predicted = torch.max(outputs.data, 1)
-#             total += labels.size(0)
-#             correct += (predicted == labels).sum().item()
-#
-#         print('Validataion accuracy is: {} %'.format(100 * correct / total))
-#         #################################################################################
-#         # TODO: Q2.b Implement the early stopping mechanism to save the model which has #
-#         # acheieved the best validation accuracy so-far.                                #
-#         #################################################################################
-#         best_model = None
-#         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-#
-#         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-#
-#     model.train()
-#
-#
-#
-# #
-# # # Iterate through all the files in the folder
-# # for dirpath, dirnames, files in os.walk(folderNam):
-# #     print(f'Found directory: {dirpath}')
-# #
-# #     for file_name in files:
-# #         music_file = os.path.join(dirpath, file_name)  # path + filename
-# #         print(music_file)
-# #         myaudio = AudioSegment.from_file(music_file, "wav")
-# #         y_pred = model(myaudio)
+        audio = audio.float()
+
+        # Forward pass
+        outputs = model(audio)
+
+        # Loss
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # print(torch.argmax(outputs, dim=1))
+        # print(labels)
+        correct += int(torch.sum(torch.argmax(outputs, dim=1) == labels))
+        total += int(labels.shape[0])
+
+    print('Accuracy : ', correct* 100/total)
